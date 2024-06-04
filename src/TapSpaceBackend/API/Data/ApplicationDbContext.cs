60ccdee1eb856c4016fb4d1c0e6973206d935013
Betellione <timurmiftyakov@gmail.com>
2024-06-04 04:21:16 +0300

    First models and db context

---
+++ src/TapSpaceBackend/API/Data/ApplicationDbContext.cs
@@ -0,0 +1,65 @@
+﻿namespace API.Data;
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Models;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Achievement> Achievements { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(x => x.Id).HasName("User_pk");
+
+            entity.Property(x => x.Name)
+                .IsRequired()
+                .HasMaxLength(64)
+                .HasColumnName("Name");
+
+            entity.Property(x => x.Level)
+                .IsRequired()
+                .HasDefaultValue(0)
+                .HasColumnName("Level");
+
+            entity.Property(x => x.Experience)
+                .IsRequired()
+                .HasDefaultValue(0)
+                .HasColumnName("Experience");
+
+            entity.Property(x => x.AmountOfDestroyedAsteroid)
+                .IsRequired()
+                .HasDefaultValue(0)
+                .HasColumnName("AmountOfDestroyedAsteroid");
+
+            entity.Property(x => x.Crystals)
+                .IsRequired()
+                .HasDefaultValue(0)
+                .HasColumnName("Crystals");
+
+            entity.Property(x => x.Gold)
+                .IsRequired()
+                .HasDefaultValue(0)
+                .HasColumnName("Gold");
+        });
+
+        modelBuilder.Entity<Achievement>(entity =>
+        {
+            entity.HasKey(x => x.Id).HasName("Achievement_pk");
+
+            entity.Property(x => x.Title)
+                .IsRequired()
+                .HasMaxLength(128);
+        });
+    }
+}
---
+++ src/TapSpaceBackend/API/Models/Achievement.cs
@@ -0,0 +1,7 @@
+﻿namespace API.Models;
+
+public class Achievement
+{
+    public long Id { get; set; }
+    public string Title { get; set; } = null!;
+}
